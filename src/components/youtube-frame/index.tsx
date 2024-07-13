c17ab4caa0786a406c9bf20a5514ea8823e3bab5
juji <him@jujiyangasli.com>
2024-07-14 04:39:03 +0700

    minimal Youtube UI [skip ci]

--- src/components/youtube-frame/index.tsx
+++ src/components/youtube-frame/index.tsx
@@ -1,14 +1,23 @@
 import { component$, useContext } from "@builder.io/qwik";
 import { VideoContext } from "~/lib/video-store";
 import styles from './style.module.css'
+import { URLSearchParams } from "url";
 
 export const YoutubeFrame = component$(() => {
 
   const { id } = useContext(VideoContext)
 
+  // https://developers.google.com/youtube/player_parameters
+  const encoded = Object.entries({
+    autoplay: '0',
+    controls: '0',
+    fs: '0',
+    iv_load_policy: '3',
+  }).map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join("&");
+
   return id ? <div class={styles.container}>
     <iframe
-    src={`https://www.youtube.com/embed/${id}`} 
+    src={`https://www.youtube.com/embed/${id}?${encoded}`} 
     title="YouTube video player" 
     frameBorder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
